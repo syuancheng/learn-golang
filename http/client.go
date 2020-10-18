0da7c0264738cfb5bf7cc4d35396e14b9df4bbed
siyuan.cheng <siyuan.cheng@shopee.com>
2020-10-18 11:46:35 +0800

    feat:http study

---
+++ http/client.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	resp, err := http.Get("https://www.imooc.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	response, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(response))
+
+}
